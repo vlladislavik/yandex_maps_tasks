a7987381a62ec97116824a6a02852becd743ec5b
vlladislavik <korolev.vladik@gmail.com>
2024-03-06 19:44:58 +0300

    С PgUp и PgDn, но очень плохо работающими

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import sys
 import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
 
 SCREEN_SIZE = [600, 450]
 
@@ -14,6 +15,9 @@ class Example(QMainWindow):
         super().__init__()
         uic.loadUi('yandex_maps.ui', self)
 
+        self.spn = None
+        self.coords = None
+
         self.btn.clicked.connect(self.getImage)
 
     def getImage(self):
@@ -22,6 +26,9 @@ class Example(QMainWindow):
 
         print(self.coords, self.spn)
 
+        self.show_map()
+
+    def show_map(self):
         map_request = ("http://static-maps.yandex.ru/1.x/?ll={},{}"
                        "&spn={},{}&l=map").format(self.coords[1], self.coords[0],
                                                   self.spn[0], self.spn[1])
@@ -40,6 +47,16 @@ class Example(QMainWindow):
         self.pixmap = QPixmap(self.map_file)
         self.map_image.setPixmap(self.pixmap)
 
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.spn = [int(self.spn[0]) + 0.02, int(self.spn[1]) + 0.02]
+            print('pgup', self.spn)
+            self.show_map()
+        if event.key() == Qt.Key_PageDown:
+            if not (int(self.spn[0]) - 0.02 < 0 or int(self.spn[1]) - 0.02 < 0):
+                self.spn = [int(self.spn[0]) - 0.0002, int(self.spn[1]) - 0.0002]
+                print('pgdn', self.spn)
+                self.show_map()
 
     def closeEvent(self, event):
         os.remove(self.map_file)
